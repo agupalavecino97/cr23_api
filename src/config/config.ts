2fa7b96c424da1b3fb42a3c3aa9f40d89b8f8b19
agustin <agupalavecino97@gmail.com>
2023-02-12 20:36:26 -0300

    change database connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ export default {
     jwtSecret: process.env.JSW_SECRET || 'secretCode123',
     DB: {
         // URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cr23',
-        URI: process.env.MONGODB_URI || 'mongodb+srv://aguapalavecino97:Palavecino+2015@cluster0.6rgje9z.mongodb.net/test',
+        URI: process.env.MONGODB_URI || 'mongodb+srv://aguapalavecino97:Palavecino+2015@cluster0.6rgje9z.mongodb.net/?retryWrites=true&w=majority',
         USER: process.env.MONGODB_USER || '',
         PASSWORD: process.env.MONGODB_PASSWORD || ''
     }
